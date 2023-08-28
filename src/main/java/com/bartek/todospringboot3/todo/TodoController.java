b2e92b1af84cd468e5a118ff284c4c1049409249
bartek <smalecbartlomiej@gmail.com>
2023-08-28 21:53:50 +0200

    Add todo

--- src/main/java/com/bartek/todospringboot3/todo/TodoController.java
+++ src/main/java/com/bartek/todospringboot3/todo/TodoController.java
@@ -5,7 +5,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.time.LocalDate;
 
 @Controller
 @AllArgsConstructor
@@ -16,5 +20,17 @@ public class TodoController {
     public String listAllTodos(ModelMap map) {
         map.put("todos", todoService.findByUsername("in28minutes"));
         return "listTodos";
+    }
+
+    @RequestMapping(value = "add-todo", method = RequestMethod.GET)
+    public String showNewTodoPAge() {
+        return "todo";
+    }
+
+    @RequestMapping(value = "add-todo", method = RequestMethod.POST)
+    public String addNewTodo(@RequestParam String description, ModelMap map) {
+        String name = (String) map.get("name");
+        todoService.addTodo(name,description ,LocalDate.now().plusYears(1), false);
+        return "redirect:list-todos";
     }
 }
